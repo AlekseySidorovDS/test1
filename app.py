715dc65f0679b57b9de952be1fa33c5aefab0053
AlekseySidorovDS <84940334+AlekseySidorovDS@users.noreply.github.com>
2021-06-01 15:25:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,10 @@ class Predict_proba (Resource):
        # X=[args["woe_prev_cls2cur_opn_dd"], args["woe_prev_max_dpd"], args["woe_prev_cred_max_dpd_lp"], args["woe_gender"], args["woe_working_industry"], args["woe_ddong_hist_found"], args["woe_prev_cic_loans"], args["woe_age"], args["woe_JS_VAR_132"], args["woe_JS_VAR_101"], args["woe_gen_mar"], args["woe_cur_cic_loans"], args["woe_JS_VAR_159"], args["woe_antifraud_score"]]
        # X = [args["woe_gender"]]
        # model_result = XG_MODEL_R1_START_V1.predict_proba(X)[1]
+        Y = args['rate']
         X = args['name']
 
-        return  X , 200
+        return  Y, X , 200
 
         
 api.add_resource(Predict_proba,"/")
