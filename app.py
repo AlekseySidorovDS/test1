484bae5798bd20664507042d3a4ebf0e3cfa7bc9
AlekseySidorovDS <84940334+AlekseySidorovDS@users.noreply.github.com>
2021-06-02 00:31:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ class Predict_proba (Resource):
         parser.add_argument('woe_JS_VAR_159')
         parser.add_argument('woe_antifraud_score')       
         args=parser.parse_args()
-        #X = args['woe_prev_cls2cur_opn_dd']
+        
         X=[args["woe_prev_cls2cur_opn_dd"], args["woe_prev_max_dpd"], args["woe_prev_cred_max_dpd_lp"], args["woe_gender"], args["woe_working_industry"], args["woe_ddong_hist_found"], args["woe_prev_cic_loans"], args["woe_age"], args["woe_JS_VAR_132"], args["woe_JS_VAR_101"], args["woe_gen_mar"], args["woe_cur_cic_loans"], args["woe_JS_VAR_159"], args["woe_antifraud_score"]]
 
         array = np.array(X)
@@ -36,8 +36,11 @@ class Predict_proba (Resource):
            ,'woe_ddong_hist_found','woe_prev_cic_loans','woe_age','woe_JS_VAR_132','woe_JS_VAR_101', 'woe_gen_mar',
           'woe_cur_cic_loans','woe_JS_VAR_159','woe_antifraud_score']
         df = pd.DataFrame(data = array.reshape(1,-1), index = [1], columns = columns) 
+        filename = 'XG_MODEL_R1_START_V1.sav'
+        model0_main = pickle.load(open(filename, 'rb'))
+
         model_result = model0_main.predict_proba(df)[0][1]
-        # model_result = XG_MODEL_R1_START_V1.predict_proba(X)[1]
+        
 
 
         return model_result  , 200
