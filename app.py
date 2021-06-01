41d72aba8da937462b8b31d223fe9bfc3a5d3057
AlekseySidorovDS <84940334+AlekseySidorovDS@users.noreply.github.com>
2021-06-01 16:53:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,18 +13,29 @@ class Predict_proba (Resource):
     
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('name')
-        parser.add_argument('test')
-        args = parser.parse_args()
-       # X=[args["woe_prev_cls2cur_opn_dd"], args["woe_prev_max_dpd"], args["woe_prev_cred_max_dpd_lp"], args["woe_gender"], args["woe_working_industry"], args["woe_ddong_hist_found"], args["woe_prev_cic_loans"], args["woe_age"], args["woe_JS_VAR_132"], args["woe_JS_VAR_101"], args["woe_gen_mar"], args["woe_cur_cic_loans"], args["woe_JS_VAR_159"], args["woe_antifraud_score"]]
-       # X = [args["woe_gender"]]
-       # model_result = XG_MODEL_R1_START_V1.predict_proba(X)[1]
-        X = args['name']
-        Y = args['test']
-        R = int(X)
-        T = int(Y)
+        parser.add_argument('prev_cls2cur_opn_dd')
+        parser.add_argument('prev_max_dpd')
+        parser.add_argument('prev_cred_max_dpd_lp')
+        parser.add_argument('gender')
+        parser.add_argument('working_industry')
+        parser.add_argument('ddong_hist_found')
+        parser.add_argument('prev_cic_loans')
+        parser.add_argument('age')
+        parser.add_argument('JS_VAR_132')
+        parser.add_argument('JS_VAR_101')
+        parser.add_argument('gen_mar')
+        parser.add_argument('cur_cic_loans')
+        parser.add_argument('JS_VAR_159')
+        parser.add_argument('antifraud_score')       
+        args=parser.parse_args()
+        X=[args['woe_prev_cls2cur_opn_dd'], args['woe_prev_max_dpd'], args['woe_prev_cred_max_dpd_lp'], args['woe_gender'], args['woe_working_industry'], args['woe_ddong_hist_found'], args['woe_prev_cic_loans'], args['woe_age'], args['woe_JS_VAR_132'], args['woe_JS_VAR_101'], args['woe_gen_mar'], args['woe_cur_cic_loans'], args['woe_JS_VAR_159'], args['woe_antifraud_score']]
+       
 
-        return  R*T , 200
+        # X = [args["woe_gender"]]
+       # model_result = XG_MODEL_R1_START_V1.predict_proba(X)[1]
+
+
+        return  X , 200
 
         
 api.add_resource(Predict_proba,"/")
